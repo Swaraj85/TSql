aad566c788aceffdd05e5fc421ff817da454d657
Swaraj <swaraj.ghosh@live.in>
2018-12-23 11:15:19 +0530

    Updated

--- Administrator.sql
+++ Administrator.sql
@@ -91,3 +91,54 @@ where pri2.name = 'swaglap\raygun'
  ************************************** Execution Context *************************************
  execute as login = 'swaglap\gort' -- impersonlization, OBO (on behalf of) server principal
  runas /profile /user:swaglap\workerbee "C:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\Ssms.exe"
+
+ ************************************** Locks *************************************
+ 
+ SELECT -- use * to explore other available attributes
+request_session_id AS spid,
+resource_type AS restype,
+resource_database_id AS dbid,
+DB_NAME(resource_database_id) AS dbname,
+resource_description AS res,
+resource_associated_entity_id AS resid,
+request_mode AS mode,
+request_status AS status
+FROM sys.dm_tran_locks;
+
+SELECT -- use * to explore
+session_id AS spid,
+connect_time,
+last_read,
+last_write,
+most_recent_sql_handle
+FROM sys.dm_exec_connections
+WHERE session_id IN(63, 66);
+
+SELECT session_id, text
+FROM sys.dm_exec_connections
+CROSS APPLY sys.dm_exec_sql_text(most_recent_sql_handle) AS ST
+WHERE session_id IN(63, 66);
+
+SELECT -- use * to explore
+session_id AS spid,
+login_time,
+host_name,
+program_name,
+login_name,
+nt_user_name,
+last_request_start_time,
+last_request_end_time
+FROM sys.dm_exec_sessions
+WHERE session_id IN(63,66);
+
+SELECT -- use * to explore
+session_id AS spid,
+blocking_session_id,
+command,
+sql_handle,
+database_id,
+wait_type,
+wait_time,
+wait_resource
+FROM sys.dm_exec_requests
+WHERE blocking_session_id > 0;
